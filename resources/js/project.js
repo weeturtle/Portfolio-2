d24a6540ec7f8ed1c3fd647cc11cecac24706155
Max London <grahamluk66@gmail.com>
2021-07-09 21:18:53 +0100

    JS Started

--- resources/js/project.js
+++ resources/js/project.js
@@ -1,2 +1,33 @@
-let project_container = document.getElementsByClassName('project-container');
-project_container.innerHTML = "<h1>Work</h1>"
+let projects = document.getElementsByClassName('project');
+let leftArrow = document.getElementById('left-arrow');
+let rightArrow = document.getElementById('right-arrow');
+
+let projects = [
+    {
+        projectName: "Holly",
+        description: "Holly is a voice controlled assistant with basic commands and the ability to control smart home devices.",
+        language: "Python",
+        startDate: "8/5/2019",
+        endDate: "incomplete"
+    },
+    {
+        projectName: ""
+    }
+];
+
+const NextProject = () => {
+    alert("Next Project");
+}
+
+const PreviousProject = () => {
+    alert("Previous Project");
+}
+
+leftArrow.addEventListener('click', event => {
+    PreviousProject();
+})
+
+rightArrow.addEventListener('click', event => {
+    NextProject();
+})
+
